585b21fbaa8ac4d921984deffcb7812d477a671c
lomilomi304 <lomilomi304@gmail.com>
2024-10-31 12:36:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// main.js
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
@@ -6,24 +5,27 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'build/icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
+
   win.loadFile('index.html')
+  
+  // Optional: Open DevTools by default (remove in production)
+  // win.webContents.openDevTools()
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
